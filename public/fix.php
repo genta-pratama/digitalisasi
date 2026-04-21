bc7dce5f984703792abe743b66501839245e8ebf
Muzakirjack <muzakkir.sbg22@gmail.com>
2026-04-21 13:05:45 +0700

    temp fix script

--- public/fix.php
+++ public/fix.php
@@ -1,22 +1,7 @@
 <?php
 $pdo = new PDO('mysql:host=storage-lab-jpb9jz;dbname=db_digitalisasi', 'digitalisasi_user', 'digital-lab-kimia');
 
-// Cek dan tambah google_id
-$cols = $pdo->query("SHOW COLUMNS FROM users LIKE 'google_id'")->fetchAll();
-if (empty($cols)) {
-    $pdo->exec("ALTER TABLE users ADD COLUMN google_id VARCHAR(255) NULL");
-    echo "google_id added!<br>";
-} else {
-    echo "google_id already exists<br>";
-}
-
-// Cek dan tambah avatar
-$cols = $pdo->query("SHOW COLUMNS FROM users LIKE 'avatar'")->fetchAll();
-if (empty($cols)) {
-    $pdo->exec("ALTER TABLE users ADD COLUMN avatar VARCHAR(255) NULL");
-    echo "avatar added!<br>";
-} else {
-    echo "avatar already exists<br>";
-}
+$pdo->exec("ALTER TABLE users MODIFY COLUMN password VARCHAR(255) NULL");
+echo "password nullable done!<br>";
 
 echo "Done!";
